35f35a0799312254b1add9f6d6bdc547708d4aee
harsh2971 <yharsh44@gmail.com>
2023-03-17 10:07:00 +0530

    Updates

---
+++ precache-manifest.82a04b8932df9c54ec51893b8fb68ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "714225362cd3e73099b6",
+    "url": "/textutils/static/css/main.e81b2669.chunk.css"
+  },
+  {
+    "revision": "714225362cd3e73099b6",
+    "url": "/textutils/static/js/main.f8157dd6.chunk.js"
+  },
+  {
+    "revision": "cb01e6ed2570e18d60b6",
+    "url": "/textutils/static/js/runtime~main.b9715171.js"
+  },
+  {
+    "revision": "2237414ef4ab2d88df04",
+    "url": "/textutils/static/js/2.e7abb95e.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/textutils/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "0cd9262fbeab9a1a776cdf8426a8211d",
+    "url": "/textutils/index.html"
+  }
+];
